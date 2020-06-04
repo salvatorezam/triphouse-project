f064936190ca4eb3334b1873ced4e4aef640d4c8
Michele07 <michele.nasser@community.unipa.it>
2020-06-04 23:06:38 +0200

    ejs

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET home page. */
+router.get('/Ricerca', function(req, res, next) {
+  res.render('Ricerca', { title: 'Express' });
+});
+
 module.exports = router;
