0b45e7f28ccac92c2cfabbce8421a8ffc3346a37
maurorusso22 <mauro.russo@community.unipa.it>
2020-06-04 23:49:58 +0200

    modifiche

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,17 +3,47 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
-/* GET home page. */
+/* GET ricerca. */
 router.get('/Ricerca', function(req, res, next) {
   res.render('Ricerca', { title: 'Express' });
 });
 
-/* GET prova. */
-router.get('/FLPE', function(req, res, next) {
-  res.render('finestraListaPrenotazioniEffettuate.ejs', { title: 'Express' });
+/* GET finestraListaPrenotazioniEffettuate */
+router.get('/finestraListaPrenotazioniEffettuate', function(req, res, next) {
+  res.render('finestraListaPrenotazioniEffettuate');
+});
+
+/* GET finestraPrenotazioneEffettuata */
+router.get('/finestraPrenotazioneEffettuata', function(req, res, next) {
+  res.render('finestraPrenotazioneEffettuata');
+});
+
+/* GET modificaDatiPersonali */
+router.get('/modificaDatiPersonali', function(req, res, next) {
+  res.render('modificaDatiPersonali');
+});
+
+/* GET finestraListaPrenotazioniRicevute */
+router.get('/finestraListaPrenotazioniRicevute', function(req, res, next) {
+  res.render('finestraListaPrenotazioniRicevute');
+});
+
+/* GET finestraPrenotazioneRicevuta */
+router.get('/finestraPrenotazioneRicevuta', function(req, res, next) {
+  res.render('finestraPrenotazioneRicevuta');
+});
+
+/* GET aggiungiAlloggio */
+router.get('/aggiungiAlloggio', function(req, res, next) {
+  res.render('aggiungiAlloggioDir/aggiungiAlloggio');
+});
+
+/* GET visualizzaAlloggio */
+router.get('/visualizzaAlloggio', function(req, res, next) {
+  res.render('visualizzaAlloggio');
 });
 
 module.exports = router;
