4a83826309d78494571d1034e4f0fa67e06acc73
maurorusso22 <mauro.russo@community.unipa.it>
2020-06-04 22:37:33 +0200

    porco zio!

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET prova. */
+router.get('/FLPE', function(req, res, next) {
+  res.render('finestraListaPrenotazioniEffettuate.ejs', { title: 'Express' });
+});
+
 module.exports = router;
