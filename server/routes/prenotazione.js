22bd8ee23f00b2907a1dc3df9adf19d8ddb04862
Edoardo Dovico <edoardo2.dov@outlook.it>
2020-06-05 17:04:53 +0200

    creazione percorsi router prenotazione

---
+++ server/routes/prenotazione.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET prenotazionePg1 */
+router.get('/prenotazionePg1', function(req, res, next) {
+    res.render('prenotazioneDir/prenotazionePg1');
+});
+
+/* GET prenotazionePg2 */
+router.get('/prenotazionePg2', function(req, res, next) {
+    res.render('prenotazioneDir/prenotazionePg2');
+});
+
+/* GET prenotazionePg3 */
+router.get('/prenotazionePg3', function(req, res, next) {
+    res.render('prenotazioneDir/prenotazionePg3');
+});
+
+/* GET prenotazionePg1 */
+router.get('/prenotazionePg4', function(req, res, next) {
+    res.render('prenotazioneDir/prenotazionePg4');
+});
+
+
+module.exports = router;
