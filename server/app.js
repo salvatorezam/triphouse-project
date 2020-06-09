83e46ae0402107839b748a884b1b5a9f07b53de6
maurorusso22 <mauro.russo@community.unipa.it>
2020-06-09 17:17:52 +0200

    aaaaaaaaaaaaaaa

--- server/app.js
+++ server/app.js
@@ -15,6 +15,7 @@ var visualizzaAlloggio = require('./routes/visualizzaAlloggio');
 
 var app = express();
 
+//AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
