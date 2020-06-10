31e01fe5b86e7eeea779cc8859626bc1e0318f98
maurorusso22 <mauro.russo@community.unipa.it>
2020-06-10 17:59:12 +0200

    profilo Utente sessione

--- server/routes/profiloUtenteControl.js
+++ server/routes/profiloUtenteControl.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var createError = require('http-errors');
+const { transporter } = require('../mailsender/mailsender-config');
+const { inviaMailHost } = require('../mailsender/mailsender-middleware');
 
-//creo degli array per salvare gli id delle prenotazioni
-var listaIdPrenConcluse = [];
-var listaIdPrenNonConcluse = [];
+//informazioni che mi serve mantenere in memoria
+var idUtente = "";
+var prenotazioni = {};
+var prenData = {};
 
 //const crypto = require('crypto');
 const { config } = require('../db/config');
@@ -17,8 +20,8 @@ async function getListaPrenotazioniEffettuate(req, res, next) {
 
   const db = await makeDb(config);
   let results = {};
-  let prenConcluse = {};
-  let prenNonConcluse = {};
+  let prenConcluse = [];
+  let prenNonConcluse = [];
   try {
       await withTransaction(db, async() => {
 
@@ -32,47 +35,62 @@ async function getListaPrenotazioniEffettuate(req, res, next) {
               console.log('Sessione Utente non trovata!');
               next(createError(404, 'Sessione Utente non trovata'));
           } else {
-              console.log(results[0]);
-              var idUtente = JSON.parse(results[0].data);
-              idUtente = idUtente.user.id_utente;
-              console.log(idUtente);
+              let datiUtente = JSON.parse(results[0].data);
+              idUtente = datiUtente.user.id_utente;
           }
           
-          let sql1 = "SELECT p.ID_PREN AS ID_PREN, p.data_inizio AS data_inizio, a.titolo AS titolo, a.citta AS citta, p.stato_prenotazione AS stato_pren \
-                      FROM Prenotazione p, Alloggio a \
-                      WHERE p.alloggio = a.ID_ALL AND p.stato_prenotazione <> 'conclusa' AND p.utente = ?;";
-          prenConcluse = await db.query(sql1, idUtente)
+          let sql1 = "SELECT p.ID_PREN AS ID_PREN, a.ID_ALL AS ID_ALL, p.data_inizio AS data_inizio, p.data_fine AS data_fine, \
+                      a.titolo AS titolo, a.indirizzo AS indirizzo, a.n_civico AS n_civico, \
+                      a.citta AS citta,  a.tipo_all AS tipo_all, uh.telefono AS telefono_uh, \
+                      a.nome_proprietario AS nome_proprietario, p.prezzo_totale AS prezzo_totale, uh.email AS email_uh, \
+                      p.stato_prenotazione AS stato_prenotazione \
+                      FROM Prenotazione p, Alloggio a , UtenteRegistrato uh \
+                      WHERE p.alloggio = a.ID_ALL AND a.proprietario = uh.ID_UR AND p.utente = ?; \
+                      SELECT p.ID_PREN AS ID_PREN, dos.nome AS nome_osp \
+                      FROM Prenotazione p, DatiOspiti dos \
+                      WHERE dos.prenotazione = p.ID_PREN; \
+                      SELECT p.ID_PREN \
+                      FROM RecensisciAlloggio ra, Alloggio a, Prenotazione p\
+                      WHERE ra.alloggio = a.ID_ALL AND p.alloggio = a.ID_ALL AND ra.scrittore = ?";
+          prenotazioni = await db.query(sql1, [idUtente, idUtente])
               .catch(err => {
                   throw err;
               });
           
-          for (el of prenConcluse) {
-            listaIdPrenConcluse.push(el.ID_PREN);
+          //unifico i risultati delle query
+          for (elPren of prenotazioni[0]) {
+
+            //ospiti
+            elPren.nomi_ospiti = "";
+            if (prenotazioni[1].length != 0) {
+              for (elDatOsp of prenotazioni[1]) {
+                if (elPren.ID_PREN == elDatOsp.ID_PREN) {
+                  elPren.nomi_ospiti = elPren.nomi_ospiti + elDatOsp.nome_osp + "-" ;
+                }
+              }
+            }
+
+            //recensioni
+            if (prenotazioni[2].length != 0) {
+              for (let elRec of prenotazioni[2]) {
+                if (elPren.ID_PREN == elRec.ID_PREN) {
+                  elPren.recBoolean = true;
+                } 
+              }
+            }
           }
 
-          let sql2 = "SELECT p.ID_PREN AS ID_PREN, p.data_inizio AS data_inizio, a.titolo AS titolo, a.citta AS citta, p.stato_prenotazione AS stato_pren \
-                      FROM Prenotazione p, Alloggio a \
-                      WHERE p.alloggio = a.ID_ALL AND p.stato_prenotazione = 'conclusa' AND p.utente = ?;";
-          prenNonConcluse = await db.query(sql2, idUtente)
-              .catch(err => {
-                  throw err;
-              });
-          
-          for (el of prenNonConcluse) {
-            listaIdPrenNonConcluse.push(el.ID_PREN);
+          prenotazioni = prenotazioni[0];
+
+          for (el of prenotazioni) {
+            if (el.stato_prenotazione == 'conclusa') {
+              prenConcluse.push(el);
+            }
+            else {
+              prenNonConcluse.push(el);
+            }
           }
-          
-          //DA CANCELLARE
-          console.log('prenCONCLUSE');
-          console.log(listaIdPrenConcluse);
-          console.log(prenConcluse.length);
-          console.log(prenConcluse);
-          console.log('prenNON');
-          console.log(listaIdPrenNonConcluse);
-          console.log(prenNonConcluse.length);
-          console.log('ecco');
-          console.log({dataNC: prenNonConcluse});
-          
+
           res.render('finestraListaPrenotazioniEffettuate', {data : {dataC: prenConcluse, dataNC: prenNonConcluse}});
       });
   } catch (err) {
@@ -84,76 +102,39 @@ async function getListaPrenotazioniEffettuate(req, res, next) {
 /* GET finestraPrenotazioneEffettuata */
 router.get('/finestraPrenotazioneEffettuata', getPrenotazioneEffettuata);
 
-async function getPrenotazioneEffettuata(req, res, next) {
+function getPrenotazioneEffettuata(req, res, next) {
 
-  const db = await makeDb(config);
-  let prenData = {};
-  let recData = {};
+  let prenDataArray = prenotazioni.filter((el) => { return el.ID_PREN == req.query.id });
+
+  prenData = prenDataArray[0];
+
   let stato_pren = "";
   let stato_rec = "";
   let stato_pag = "";
 
-  try {
-      await withTransaction(db, async() => {
-          
-          //dati prenotazione che mi servono
-          let sql = "SELECT p.data_inizio AS data_inizio, p.data_fine AS data_fine, \
-                      a.titolo AS titolo, a.indirizzo AS indirizzo, a.n_civico AS n_civico, \
-                      a.citta AS citta, ur.telefono AS telefono_prp, a.tipo_all AS tipo_all, \
-                      a.nome_proprietario AS nome_proprietario, p.prezzo_totale AS prezzo_totale, ur.email AS email_prp, \
-                      p.stato_prenotazione AS stato_prenotazione \
-                      FROM Prenotazione p, Alloggio a, UtenteRegistrato ur \
-                      WHERE p.alloggio = a.ID_ALL AND a.proprietario = ur.ID_UR AND a.titolo = 'Casa Roma';";
-          prenData = await db.query(sql)
-              .catch(err => {
-                  throw err;
-              });
-          
-          //check per verificare la possibilità di annullare la prenotazione o pagare
-          if (prenData[0].stato_prenotazione == 'conclusa') {
+  //check per verificare la possibilità di annullare la prenotazione o pagare
+  if (prenData.stato_prenotazione == 'conclusa') {
+    // METTERCI ANCHE LA DATA DI SCADENZA 
 
-            // METTERCI ANCHE LA DATA DI SCADENZA 
-            //disabilita tasto annullamento
-            stato_pren = "disabled";
-
-            //disabilita tasto pagamento
-            stato_pag = "disabled";
-          }
-
-          //check per verificare la possibilità di pagamento
-          if (prenData[0].stato_prenotazione == 'richiesta' || prenData[0].stato_prenotazione == 'pagata') {
-
-            stato_pag = "disabled";
-          }
-          
-          //dati recensione                     METTERE TUTTE LE SQL ASSIEME
-          let sql_rec = "SELECT p.ID_PREN \
-                          FROM Prenotazione p, Alloggio a, RecensisciAlloggio ra \
-                          WHERE p.alloggio = a.ID_ALL AND ra.alloggio = a.ID_ALL AND a.titolo = 'Casa Roma';";
-          recData = await db.query(sql_rec)
-              .catch(err => {
-                  throw err;
-              });
-          
-          //check per verificare la possibilità di recensire (in base a recensioni già fatte o conclusione vacanza)
-          if (recData.length != 0 || prenData[0].stato_prenotazione != 'conclusa') {
-
-            stato_rec = "disabled";
-          }
-          
-          //DA CANCELLARE
-          console.log('DATI PRENOTAZIONE: ');
-          console.log(prenData);
-
-          console.log('DATI RECENSIONE: ');
-          console.log(stato_rec);
-          
-          res.render('finestraPrenotazioneEffettuata', {data : {data_pren : prenData, data_rec : stato_rec, data_ann : stato_pren, data_pag : stato_pag}});
-      });
-  } catch (err) {
-      console.log(err);
-      next(createError(500));
+    //disabilita tasto annullamento
+    stato_pren = "disabled";
+    //disabilita tasto pagamento
+    stato_pag = "disabled";
   }
+
+  //check per verificare la possibilità di pagamento
+  if (prenData.stato_prenotazione == 'richiesta' || prenData.stato_prenotazione == 'pagata') {
+
+    stato_pag = "disabled";
+  }
+
+  //check per verificare la possibilità di recensire (in base a recensioni già fatte o conclusione vacanza)
+  if (prenData.stato_prenotazione != 'conclusa' || prenData.recBoolean == true) {
+    stato_rec = "disabled";
+  }
+
+  console.log(prenData);
+  res.render('finestraPrenotazioneEffettuata', {data : {data_pren : prenData, data_rec : stato_rec, data_ann : stato_pren, data_pag : stato_pag}});    
 }
   
 
@@ -180,8 +161,8 @@ async function recensisciAlloggio(req, res, next) {
           let values = [
             req.body.testoRec,
             today,
-            'scrittore_rec',
-            '2d8cdeb2-a755-11ea-b30a-a066100a22be',
+            idUtente,
+            prenData.ID_ALL,
             req.body.valutazione
           ];
           recensione = await db.query(sql, values)
@@ -189,12 +170,15 @@ async function recensisciAlloggio(req, res, next) {
                   throw err;
               }); 
           
+          prenData.recBoolean = true;
+          
           console.log(req.body);
           //DA CANCELLARE
           console.log('DATI RECENSIONE: ');
           console.log(values);
           
-          res.redirect('/profiloUtenteControl/finestraPrenotazioneEffettuata');
+          let link = '/profiloUtenteControl/finestraPrenotazioneEffettuata?id=' + prenData.ID_PREN;
+          res.redirect(link);
       });
   } catch (err) {
       console.log(err);
@@ -208,6 +192,7 @@ router.get('/annullaPrenotazione', annullaPrenotazione);
 async function annullaPrenotazione(req, res, next) {
 
   const db = await makeDb(config);
+  var today = new Date();
 
   try {
     await withTransaction(db, async() => {
@@ -215,14 +200,24 @@ async function annullaPrenotazione(req, res, next) {
       //MODIFICARE SQL ASSOLUTAMENTE
       let sql = "UPDATE Prenotazione \
                   SET stato_prenotazione = 'conclusa' \
-                  WHERE ID_PREN = '485ae14d-a756-11ea-b30a-a066100a22be';";
-      annullamento = await db.query(sql)
+                  WHERE ID_PREN = ?; \
+                  INSERT INTO RecensisciAlloggio VALUES (UUID(),'prenotazione annullata',?,?,?,null);";
+      annullamento = await db.query(sql, [prenData.ID_PREN, today, idUtente, prenData.ID_ALL])
           .catch(err => {
               throw err;
           });
 
+      prenData.stato_prenotazione = 'conclusa';
+      prenData.recBoolean = true;
+
       console.log('Prenotazione annullata');
-      res.redirect('/profiloUtenteControl/finestraPrenotazioneEffettuata');
+
+      // Invio della mail di conferma annullamento
+      let testo = "La prenotazione n." + prenData.ID_PREN + " è stata annullata dal cliente";
+      inviaMailHost(transporter, prenData.email_uh, testo).catch(err => {throw err;});
+
+      let link = '/profiloUtenteControl/finestraPrenotazioneEffettuata?id=' + prenData.ID_PREN;
+      res.redirect(link);
     });
   } catch (err) {
       console.log(err);
