92474bf3dad47f0a6e8406cb776b62e1c844b1d6
Michele07 <michele.nasser@community.unipa.it>
2020-06-12 22:45:43 +0200

    okokk

--- server/routes/ricercaControl.js
+++ server/routes/ricercaControl.js
@@ -3,7 +3,8 @@ var express = require('express');
 var router = express.Router();
 
 var arrayAlloggi = [];
-var contatoreAlloggi;
+var inizioArray;
+var fineArray;
 
 
 /*carichiamo il middleware*/
@@ -46,11 +47,16 @@ async function listaRicerca(req, res, next) {
             
         arrayAlloggi = results1;
 
-        (arrayAlloggi.length > 6 ? contatoreAlloggi = 6 : 0); 
-        console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
-        console.log(arrayAlloggi);
+        if(arrayAlloggi.length > 6){
+            inizioArray = 0;
+            fineArray = 6;
+        }
+        else{
+            inizioArray = 0;
+            fineArray = arrayAlloggi.length;
+        }
 
-        res.render('listaRicerca', {data : {array : arrayAlloggi, contatore : contatoreAlloggi}});
+        res.render('listaRicerca', {data : {array : arrayAlloggi, inizioCount : inizioArray, fineCount : fineArray}});
     
       });
     } catch (err) {
@@ -65,31 +71,35 @@ router.get('/paginaSuccessiva', paginaSuccessiva);
 
 async function paginaSuccessiva(req,res,next){
 
-    contatoreAlloggi = arrayAlloggi.length - contatoreAlloggi;
+    inizioArray = fineArray;
 
-    res.render('listaRicerca', {data : {array : arrayAlloggi, contatore : contatoreAlloggi}})
+    if(arrayAlloggi.length > (fineArray + 6)){
+
+        fineArray = fineArray + 6;
+    }
+    else{
+        fineArray = arrayAlloggi.length;
+    }
+
+    res.render('listaRicerca', {data : {array : arrayAlloggi, inizioCount : inizioArray, fineCount : fineArray}})
 
 }
+
+/*GET paginaPrecedente*/
 
 router.get('/paginaPrecedente', paginaPrecedente);
 
 async function paginaPrecedente(req,res,next){
 
-    contatoreAlloggi = contatoreAlloggi - 6;
 
-    res.render('listaRicerca', {data : {array : arrayAlloggi, contatore : contatoreAlloggi}})
+    fineArray = inizioArray;
+
+    inizioArray = inizioArray - 6;
+
+
+    res.render('listaRicerca', {data : {array : arrayAlloggi, inizioCount : inizioArray, fineCount : fineArray}})
 
 }
 
 
 module.exports = router;
-
-/*SELECT a.ID_ALL AS ID_ALL ,a.proprietario AS proprietario,a.tipo_all AS tipo_all,a.nome_proprietario AS nome_proprietario,a.indirizzo AS indirizzo ,a.n_civico AS n_civico,a.cap AS cap,a.regione AS regione ,a.citta AS citta,a.provincia AS provincia, \
-                    a.num_ospiti_max AS num_ospiti_max,a.distanza_centro AS distanza_centro,a.num_letti_singoli AS num_letti_singoli,a.num_letti_matrimoniali AS num_letti_matrimoniali,a.num_camere AS num_camere ,a.num_bagni AS num_bagni,a.cucina AS cucina,a.lavanderia AS lavanderia, \
-                    a.aria_condizionata AS aria_condizionata,a.wifi AS wifi,a.colazione AS colazione,a.asciugacapelli AS asciugacapelli,a.tv AS tv,a.carta_igienica AS carta_igienica,a.sapone_mani_corpo AS sapone_mani_corpo,a.asciugamano AS asciugacapelli,a.accappatoio AS accappatoio,a.cuscino AS cuscino,\
-                    a.lenzuola AS lenzuola,a.titolo AS titolo,a.descrizione_alloggio AS descrizione_alloggio,a.descrizione_regole AS descrizione_regole,a.note AS note,a.tasse AS tasse,a.prezzo AS prezzo,a.foto_0 AS foto_0,a.foto_1 AS foto_1,a.foto_2 AS foto_2,a.foto_3 As foto_3,a.foto_4 AS foto_4,a.foto_5 AS foto_5\
-                    FROM Alloggio a, datedisponibili d \
-                    WHERE a.citta = ? AND a.num_ospiti_max > ? AND a.ID_ALL NOT IN (SELECT d.alloggio\
-                                                                                    FROM datedisponibili d\
-                                                                                    WHERE (? >= d.data_inizio AND ? <= d.data_fine ) OR (? > d.data_inizio AND ? < data_fine) OR ( ? < d.data_inizio AND ? > d.data_fine ));
-    */
