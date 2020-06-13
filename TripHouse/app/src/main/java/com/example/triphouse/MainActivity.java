4cb4933921631bbba6f9e0a39ad7d4d4de3723c8
maurorusso22 <mauro.russo@community.unipa.it>
2020-06-14 00:00:14 +0200

    app android

---
+++ TripHouse/app/src/main/java/com/example/triphouse/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.triphouse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webview = (WebView) findViewById(R.id.webview);
+        webview.setWebViewClient(new WebViewClient());
+        webview.loadUrl("http://1c7b4f32aa0a.ngrok.io");
+
+        WebSettings webSettings = webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webview.canGoBack()) {
+            webview.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
